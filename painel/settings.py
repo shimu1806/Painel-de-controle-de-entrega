88f033387d92d07a4382c8ffc94d4cd6d5e8d40c
shimu1806 <samuelbrito190@gmail.com>
2024-05-04 16:20:13 -0300

    HTML e CSS base

--- painel/settings.py
+++ painel/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_URL = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 
 ]
