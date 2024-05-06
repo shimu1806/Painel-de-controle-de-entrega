e25e94b7b6e4c71b3bfd24462f812a066d5248e2
shimu1806 <samuelbrito190@gmail.com>
2024-05-06 16:04:15 -0300

    Definição HTML e CSS v1

--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'api/index.html')
+    response    = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data        = response.json()
+
+    return render(request, 'api/index.html', {'data': data[:10] })
+    
+
+
