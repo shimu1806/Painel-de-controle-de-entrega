c2b5a37a5ac8e7eea1f9ecb0c2ada50872db489f
shimu1806 <samuelbrito190@gmail.com>
2024-05-06 17:10:36 -0300

    + dataTable

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ def index(request):
     response    = requests.get('https://jsonplaceholder.typicode.com/todos')
     data        = response.json()
 
-    return render(request, 'api/index.html', {'data': data[:10] })
+    return render(request, 'api/index.html', {'data': data})
     
 
 
