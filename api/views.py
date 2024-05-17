f20aecfc09cbae7f7b8a677a3b23d949b2604c0a
shimu1806 <samuelbrito190@gmail.com>
2024-05-17 14:29:12 -0300

    sec

--- api/views.py
+++ api/views.py
@@ -1,20 +1,18 @@
 import requests
 import json
+
 from .models import Produto
 from .serializer import Produto_serializer as _Serial
+
 from django.db import transaction
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from django.core.paginator import Paginator
 from django.http import JsonResponse
 
 #@login_required(login_url="api/login")
-def index(request):    
-    # Gera queryset do banco de dados e faz display no html
-    
-
+def index(request):
     return render(request, 'api/index.html')
 
 
@@ -28,79 +26,14 @@ def fetch_data(request):
             "ORDEM": "CB7_STATUS, CB7_ORDSEP, CB7_PEDIDO"
     }
     response = requests.post('http://suntechsupplies170773.protheus.cloudtotvs.com.br:1907/rest/restqry', json=payload)
-    
-    # Registro de dados Novos
-
+    # Parse the JSON response
     queryset_response = response.json()
-    aux = []
-    new_records_count = 0
-
-    with transaction.atomic():
-        for produto in queryset_response['RETORNOS']:
-            try:
-                # Tenta obter o produto do banco de dados pelo CB7_ORDSEP
-                obj = Produto.objects.get(CB7_ORDSEP=produto["CB7_ORDSEP"])
-                # Atualiza o objeto com os novos dados
-                obj.CB7_STATUS = produto["CB7_STATUS"]
-                obj.CB7_FILIAL = produto["CB7_FILIAL"]
-                obj.CB7_PEDIDO = produto["CB7_PEDIDO"]
-                obj.CB7_CLIENT = produto["CB7_CLIENT"]
-                obj.CB7_LOJA = produto["CB7_LOJA"]
-                obj.A1_NOME = produto["A1_NOME"]
-                obj.CB7_DTEMIS = produto["CB7_DTEMIS"]
-                obj.CB7_HREMIS = produto["CB7_HREMIS"]
-                obj.CB7_DTINIS = produto["CB7_DTINIS"]
-                obj.CB7_HRINIS = produto["CB7_HRINIS"]
-                obj.CB7_DTFIMS = produto["CB7_DTFIMS"]
-                obj.CB7_HRFIMS = produto["CB7_HRFIMS"]
-                obj.CB7_NOTA = produto["CB7_NOTA"]
-                obj.CB7_SERIE = produto["CB7_SERIE"]
-                obj.CB8_PROD = produto["CB8_PROD"]
-                obj.B1_DESC = produto["B1_DESC"]
-                obj.LINE = produto["LINE"]
-                obj.save()
-                new_records_count += 1
-                print(f"Produto {produto['CB7_ORDSEP']} atualizado no banco de dados.")
-            except Produto.DoesNotExist:
-                # Se o produto não existir no banco de dados, crie um novo
-                obj = Produto(
-                    CB7_STATUS=produto["CB7_STATUS"],
-                    CB7_FILIAL=produto["CB7_FILIAL"],
-                    CB7_ORDSEP=produto["CB7_ORDSEP"],
-                    CB7_PEDIDO=produto["CB7_PEDIDO"],
-                    CB7_CLIENT=produto["CB7_CLIENT"],
-                    CB7_LOJA=produto["CB7_LOJA"],
-                    A1_NOME=produto["A1_NOME"],
-                    CB7_DTEMIS=produto["CB7_DTEMIS"],
-                    CB7_HREMIS=produto["CB7_HREMIS"],
-                    CB7_DTINIS=produto["CB7_DTINIS"],
-                    CB7_HRINIS=produto["CB7_HRINIS"],
-                    CB7_DTFIMS=produto["CB7_DTFIMS"],
-                    CB7_HRFIMS=produto["CB7_HRFIMS"],
-                    CB7_NOTA=produto["CB7_NOTA"],
-                    CB7_SERIE=produto["CB7_SERIE"],
-                    CB8_PROD=produto["CB8_PROD"],
-                    B1_DESC=produto["B1_DESC"],
-                    LINE=produto["LINE"]
-                )
-                obj.save()
-                new_records_count =+ 1
-                print(f"Creating product with CB7_ORDSEP: {produto['CB7_ORDSEP']}")
-
-
-        print(f"{new_records_count} new products created")
-
-
-
-def endpoint_fetch_data(request):
-    # endpoint JSON da db a qual alimenta a tabela
-
-    obj_created = Produto.objects.all()
-    produto_serializer = _Serial(obj_created, many=True)
-
-    return JsonResponse(produto_serializer.data, safe=False)
     
-
+    # Extract the items within the 'RETORNOS' key
+    return_items = queryset_response.get('RETORNOS', [])
+    
+    # Return the extracted items as a JsonResponse
+    return JsonResponse(return_items, safe=False)
 
 
 @csrf_exempt
@@ -117,8 +50,8 @@ def login(request):
             return render(request, 'api/login.html')
     else:
         return render(request, 'api/login.html')
-    
-    
+
+
 @csrf_exempt
 def update_status_counts(request):
     fetch_data(request)
